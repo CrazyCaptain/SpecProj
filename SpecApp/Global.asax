42d56ab7f8b9c68629f06f0a1c0585aa925b545c
ralphlaurenagustin <ralph.agustin@spectres.solutions>
2017-09-13 13:38:29 +0800

    Added Data Model

---
+++ SpecApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpecApp.MvcApplication" Language="C#" %>
---
+++ SpecApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpecApp.Startup))]
+namespace SpecApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
